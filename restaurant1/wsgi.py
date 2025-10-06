da95fbc5e406443238f4fe2b74b91f4c46f1df5d
Pedro Chacón <pedrochacon9621@gmail.com>
2025-10-06 16:31:34 +0200

    vercel 2

--- restaurant1/wsgi.py
+++ restaurant1/wsgi.py
@@ -13,5 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant1.settings')
 
-#application = get_wsgi_application()
-app = get_wsgi_application() 
+application = get_wsgi_application()  # Para Render
+app = application                     # Para Vercel
+
